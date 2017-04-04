a346787fc08937af6790d89f67f0b08a867faf4f
Paolo Dávila <pdavila@iesebre.com>
2017-04-04 17:03:59 +0200

    Implement componets working in view OK

---
+++ resources/assets/js/components/Wizard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="nav-tabs-custom">
+        <ul class="nav nav-tabs nav-justified">
+            <li v-for="step in steps" :class="{'active':step.active}">
+                <a :href="step.link" :aria-controls="step.id" data-toggle="tab">{{ step.title }}</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <slot></slot>
+        </div>
+        <!-- /.tab-content -->
+        <div class="box-footer">
+            <button type="submit" class="btn btn-primary btn-flat pull-left">Previous</button>
+            <button type="submit" class="btn btn-primary btn-flat pull-right">Next</button>
+        </div>
+        <!-- /.button-->
+    </div>
+    <!-- nav-tabs-custom -->
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        steps: [],
+        currentStep: 1
+      }
+    },
+    mounted () {
+      console.log('Component mounted')
+        this.$children.forEach( step => {
+          this.steps.push ({
+            'id': step.id,
+            'link': step.link,
+            'title': step.title,
+            'active': step.active
+          });
+        })
+    },
+    methods: {
+      stepChanged: function (step) {
+        this.currentStep = step
+      }
+    }
+  }
+</script>
--- resources/views/adminlte.blade.php
+++ resources/views/adminlte.blade.php
@@ -8,94 +8,31 @@
 @section('main-content')
     <div class="container-fluid spark-screen">
         <div class="row">
-            <div class="progress progress-sm active">
-                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-                    <span class="sr-only">20% Complete</span>
+            <div class="col-md-12">
+                <div class="progress progress-sm active">
+                    <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                        <span class="sr-only">20% Complete</span>
+                    </div>
                 </div>
+
+                <wizard>
+                    <wizard-step title="Usuari" active="true">
+                        Step 1: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, autem dolorem, doloremque doloribus expedita fugit inventore, ipsa itaque molestias nihil pariatur possimus quidem saepe sed sit ut veritatis vero voluptas.
+                    </wizard-step>
+                    <wizard-step title="Dades personals">
+                        Step 2: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur dicta dolorum ducimus, impedit, in incidunt ipsa itaque, maiores nulla quaerat quam quod repellendus repudiandae velit. Aliquid deleniti eos magni?
+                    </wizard-step>
+                    <wizard-step title="Estudis i curs">
+                        Step 3: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet earum ipsum natus, quo suscipit veritatis voluptas! Delectus distinctio eligendi impedit, maxime, mollitia nesciunt quae, quo reiciendis soluta tempore vero voluptatibus!
+                    </wizard-step>
+                    <wizard-step title="Moduls professionals">
+                        Step 4: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias facere ipsa laudantium libero necessitatibus sed, vitae voluptatem. Deleniti harum laborum laudantium maxime nam ratione rem repellat reprehenderit sit suscipit!
+                    </wizard-step>
+                    <wizard-step title="Unitats formatives">
+                        Step 5: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dolor eum harum non nulla. Beatae iusto nulla, officiis perferendis porro quam unde. Eligendi, est officiis. Possimus, praesentium voluptate? Dignissimos, quisquam?
+                    </wizard-step>
+                </wizard>
             </div>
-
-            <wizard>
-                <wizard-step title="Usuari" active="true">
-                    Step 1: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, autem dolorem, doloremque doloribus expedita fugit inventore, ipsa itaque molestias nihil pariatur possimus quidem saepe sed sit ut veritatis vero voluptas.
-                </wizard-step>
-                <wizard-step title="Dades personals">
-                    Step 2: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur dicta dolorum ducimus, impedit, in incidunt ipsa itaque, maiores nulla quaerat quam quod repellendus repudiandae velit. Aliquid deleniti eos magni?
-                </wizard-step>
-                <wizard-step title="Estudis i curs">
-                    Step 3: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet earum ipsum natus, quo suscipit veritatis voluptas! Delectus distinctio eligendi impedit, maxime, mollitia nesciunt quae, quo reiciendis soluta tempore vero voluptatibus!
-                </wizard-step>
-                <wizard-step title="Moduls professionals">
-                    Step 4: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias facere ipsa laudantium libero necessitatibus sed, vitae voluptatem. Deleniti harum laborum laudantium maxime nam ratione rem repellat reprehenderit sit suscipit!
-                </wizard-step>
-                <wizard-step title="Unitats formatives">
-                    Step 5: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dolor eum harum non nulla. Beatae iusto nulla, officiis perferendis porro quam unde. Eligendi, est officiis. Possimus, praesentium voluptate? Dignissimos, quisquam?
-                </wizard-step>
-            </wizard>
-
-            <!-- Custom Tabs -->
-            <div class="nav-tabs-custom">
-                <ul class="nav nav-tabs nav-justified">
-                    <li class="active"><a href="#tab_1" data-toggle="tab">Tab 1</a></li>
-                    <li><a href="#tab_2" data-toggle="tab">Tab 2</a></li>
-                    <li><a href="#tab_3" data-toggle="tab">Tab 3</a></li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                            Dropdown <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a>
-                            </li>
-                            <li role="presentation" class="divider"></li>
-                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <div class="tab-content">
-                    <div class="tab-pane active" id="tab_1">
-                        <b>How to use:</b>
-
-                        <p>Exactly like the original bootstrap tabs except you should use
-                            the custom wrapper <code>.nav-tabs-custom</code> to achieve this style.</p>
-                        A wonderful serenity has taken possession of my entire soul,
-                        like these sweet mornings of spring which I enjoy with my whole heart.
-                        I am alone, and feel the charm of existence in this spot,
-                        which was created for the bliss of souls like mine. I am so happy,
-                        my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
-                        that I neglect my talents. I should be incapable of drawing a single stroke
-                        at the present moment; and yet I feel that I never was a greater artist than now.
-                    </div>
-                    <!-- /.tab-pane -->
-                    <div class="tab-pane" id="tab_2">
-                        The European languages are members of the same family. Their separate existence is a myth.
-                        For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
-                        in their grammar, their pronunciation and their most common words. Everyone realizes why a
-                        new common language would be desirable: one could refuse to pay expensive translators. To
-                        achieve this, it would be necessary to have uniform grammar, pronunciation and more common
-                        words. If several languages coalesce, the grammar of the resulting language is more simple
-                        and regular than that of the individual languages.
-                    </div>
-                    <!-- /.tab-pane -->
-                    <div class="tab-pane" id="tab_3">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-                        like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                    <!-- /.tab-pane -->
-                </div>
-                <!-- /.tab-content -->
-                <div class="box-footer">
-                    <button type="submit" class="btn btn-primary btn-flat pull-left">Previous</button>
-                    <button type="submit" class="btn btn-primary btn-flat pull-right">Next</button>
-                </div>
-                <!-- /.button-->
-            </div>
-            <!-- nav-tabs-custom -->
         </div>
     </div>
 @endsection
