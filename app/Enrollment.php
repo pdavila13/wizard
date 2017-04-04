f647f9c3faa30fb0858caf6ab1538a32991980ce
Paolo Dávila <pdavila@iesebre.com>
2017-04-04 19:37:15 +0200

    Added relationshit between Enrollment and User

--- app/Enrollment.php
+++ app/Enrollment.php
@@ -2,9 +2,27 @@
 
 namespace App;
 
+use Acacha\Stateful\Contracts\Stateful;
+use Acacha\Stateful\Traits\StatefulTrait;
 use Illuminate\Database\Eloquent\Model;
 
-class Enrollment extends Model
+class Enrollment extends Model implements Stateful
 {
-    //
+    use StatefulTrait;
+
+    /*
+     * Get the user record associated with the user
+     */
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+
+    /*
+     * Get the person record associated with the person
+     */
+    public function person()
+    {
+        return $this->hasOne('App\Person');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*
+     * Get the enrollment record associated with the enrollment
+     */
+    public function enrollment()
+    {
+        return $this->hasOne('App\Enrollment');
+    }
 }
